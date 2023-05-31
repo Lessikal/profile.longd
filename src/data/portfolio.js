1a20da2a10e8f0b412a33b16624612d35c3b4ab8
Lessikal <lessikal@gmail.com>
2023-05-31 12:18:19 +0100

    photo fix1

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -7,6 +7,10 @@ import PortImg6 from "../assets/images/resources/6.jpg";
 import PortImg7 from "../assets/images/resources/7.jpg";
 import PortImg8 from "../assets/images/resources/8.jpg";
 import PortImg9 from "../assets/images/resources/9.jpg";
+import PortImg10 from "../assets/images/resources/10.jpg";
+import PortImg11 from "../assets/images/resources/11.jpg";
+import PortImg12 from "../assets/images/resources/12.jpg";
+import PortImg13 from "../assets/images/resources/13.jpg";
 
 export const portfolio = [
   {
@@ -26,6 +30,24 @@ export const portfolio = [
     portTitle: "Shopify",
     portSubTitle: "Using Shopify platform, confing env and implement firebase reltimebase and use restful api",
     portcate: "Pixel Perfect and Full Stack",
+  },
+  {
+    id: 12,
+    url: "https://apps.apple.com/ZA/app/id1533061853?mt=8",
+    portBox: "basis-full md:basis-[50%] px-[.9375rem]",
+    portImg: PortImg12,
+    portTitle: "Flash - Mobile Wallet ",
+    portSubTitle: "Minor bug fixes and a shiny new app icon to make it easier to identify your favourite app.",
+    portcate: "Flutter",
+  },
+  {
+    id: 13,
+    url: "https://apps.apple.com/app/upaychat/id1548246385?platform=iphone",
+    portBox: "basis-full md:basis-[50%] px-[.9375rem]",
+    portImg: PortImg13,
+    portTitle: "Upaychat",
+    portSubTitle: "Pay and get paid for anything. Add a note to each payment to share and connect with friends.",
+    portcate: "React Native",
   },
   {
     id: 3,
@@ -90,4 +112,23 @@ export const portfolio = [
     portSubTitle: "connect api and design pixel perfectly",
     portcate: "graphic design and api connection",
   },
+  {
+    id: 10,
+    url: "https://movingcompanylist.com/",
+    portBox: "basis-full md:basis-[33.33%] px-[.9375rem]",
+    portImg: PortImg10,
+    portTitle: "Moving and Shipping",
+    portSubTitle: "Top moving comapanies in 2023",
+    portcate: "Full Stack",
+  },
+  {
+    id: 11,
+    url: "https://play.google.com/store/apps/details?id=com.comiere.pk",
+    portBox: "basis-full md:basis-[50%] px-[.9375rem]",
+    portImg: PortImg11,
+    portTitle: "Comiere doctor booking",
+    portSubTitle: "Top app in 2023",
+    portcate: "Flutter",
+  },
+
 ];
